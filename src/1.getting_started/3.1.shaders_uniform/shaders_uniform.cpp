4a3538b1f40337a788f288ffbaa8007693751343
Han Yingju <hanyingju@live.cn>
2025-05-11 21:42:13 +0800

    4.1.textures

--- src/1.getting_started/3.1.shaders_uniform/shaders_uniform.cpp
+++ src/1.getting_started/3.1.shaders_uniform/shaders_uniform.cpp
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-
+#include <math.h>
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
